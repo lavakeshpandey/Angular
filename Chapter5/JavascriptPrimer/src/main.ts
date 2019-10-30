ff4573f2c42dc7fc2b763e8619f8f07af3b82d28
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-30 23:16:46 +0530

    end of the chapter 5, 6

--- Chapter5/JavascriptPrimer/src/main.ts
+++ Chapter5/JavascriptPrimer/src/main.ts
@@ -1,21 +1,12 @@
-// Chapter 5 starts
-let myData = new Object();
-myData.name = 'Adam';
-myData.weather = 'sunny';
-console.log('Hello ' + myData.name + '.');
-console.log('Today is ' + myData.weather + '.');
-
-// object literal format
-let myData1 = { name: 'Lavakesh', weather: 'sunny' };
-console.log('Hello ' + myData1.name + '. ');
-console.log('Today is '+ myData1.weather + '.');
-// methods : functions defined on an object
-let myData3 = {
-  name: 'Lavakesh',
-  weather: 'raining',
-  printMessages: function(){
-    console.log('Hello ' + this.name + '.');
-    console.log('It is ' + this.weather + '.');
+class MyClass {
+  constructor(name, weather) {
+    this.name = name;
+    this.weather = weather;
   }
-};
-myData3.printMessages();
+  printMessage() {
+    console.log('Hello ' + this.name + '.');
+    console.log('Today is ' + this.weather + '.');
+  }
+}
+let myData = new MyClass('Lavakesh', 'sunny');
+myData.printMessage();
