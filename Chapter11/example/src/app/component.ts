420d87104228a022e5c0e8738379bc034722839b
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-11-17 16:48:54 +0530

    added code for databind exercises till class binding

--- Chapter11/example/src/app/component.ts
+++ Chapter11/example/src/app/component.ts
@@ -7,4 +7,17 @@ import { Model } from './repository.model';
 })
 export class ProductComponent {
     model: Model = new Model();
+
+    getClasses(key: number): string {
+        const product = this.model.getProduct(key);
+        return 'p-2 ' + (product.price < 50 ? 'bg-info' : 'bg-warning');
+    }
+
+    getClassMap(key: number): Object {
+        const product = this.model.getProduct(key);
+        return {
+            'text-center bg-danger': product.name == 'Kayak',
+            'bg-info': product.price < 50
+        };
+    }
 }
